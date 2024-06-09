32af746c984fe6845112b3b13e5bf972395b4795
David Fung <davidfung@amgcomputing.com>
2024-06-08 22:46:26 -0700

    Ex 4.7 work-in-progress

---
+++ ch04/ex0407.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+var desc = `Modify reverse() to reverse the characters of a []byte slice that represents a
+UTF-8-encoded string, in place.  Can you do it without allocating new memory?
+`
+
+func main() {
+	fmt.Println("Ex 4.7")
+	fmt.Println(desc)
+}
